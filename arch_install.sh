01045d95ae3c196f468657086cb5d80d1724d72e
DDman7 <84295329+DDman7@users.noreply.github.com>
2022-03-01 00:28:33 +1100

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -28,22 +28,3 @@ pacstrap /mnt base linux linux-firmware networkmanager # Download and install th
 genfstab -U /mnt >> /mnt/etc/fstab # No idea
 
 arch-chroot /mnt # Change root to the specified directory
-
-ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime # Set the time zone
-
-hwclock --systohc # To generate /etc/adjtime
-
-locale-gen # Generate locales
-
-touch /etc/locale.conf # Create the locale.conf file
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf # Append the specified text to the specified file.
-
-(
-echo 1999
-echo 1999
-) | passwd # Setting the root user password
-
-pacman -S grub efibootmgr # Downloading and installing the boot loader
-
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot # Install the grub boot loader
-grub-mkconfig -o /boot/grub/grub.cfg # Create the grub config file
