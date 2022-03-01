4c8cc57580f86c5bd219d10cd616f466c710b589
DDman7 <84295329+DDman7@users.noreply.github.com>
2022-03-01 22:24:58 +1100

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -18,16 +18,16 @@ echo   # Last sector (Accept default: varies)
 echo w # Write changes
 ) | fdisk /dev/vda # Pipe all those inputs into fdisk for the appropriate device
 
-mkfs.fat -F32 /dev/vda1 && mkfs.ext4 /dev/vda2 # Create file systems for each partition
+mkfs.fat -F32 /dev/vda1 # Create a fat32 file system for the EFI partition
+mkfs.ext4 /dev/vda2 # Create an ext4 file system for the root partition
 
 pacman -Syy # Sync the pacman repo
-
-pacman -S reflector --noconfirm # Download and install the reflector tool
+pacman -S --noconfirm reflector # Download and install the reflector tool
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak # Create a backup of the default mirror list
 reflector -c "AU" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist # Use the best mirror I guess?
 
 mount /dev/vda2 /mnt # Mount the root partition to /mnt
-pacstrap /mnt base linux linux-firmware nano # Download and install the specified software packages to the root partition mounted at /mnt
+pacstrap /mnt base linux # Download and install the specified software packages to the root partition mounted at /mnt
 genfstab -U /mnt >> /mnt/etc/fstab # Generate an fstab file to define how disk partitions, block devices or remote file systems are mounted into the system.
 
 arch-chroot /mnt # Change root to the specified directory
