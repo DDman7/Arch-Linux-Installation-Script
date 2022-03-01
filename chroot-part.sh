8f17efecbea803631ce76a250be9ca92cf1febad
DDman7 <84295329+DDman7@users.noreply.github.com>
2022-03-01 22:50:29 +1100

    Update chroot-part.sh

--- chroot-part.sh
+++ chroot-part.sh
@@ -7,12 +7,11 @@ hwclock --systohc # To generate /etc/adjtime
 locale-gen # Generate locales
 
 touch /etc/locale.conf # Create the locale.conf file
-echo "LANG=en_AU.UTF-8" >> /etc/locale.conf # Append the specified text to the specified file.
-export LANG=en_AU.UTF-8
+echo LANG=en_AU.UTF-8 >> /etc/locale.conf # Append the specified text to the specified file.
+export LANG=en_AU.UTF-8 # No idea
 
 echo david-danyal > /etc/hostname # Set my network hostname
 touch /etc/hosts # Create the hosts file
-
 echo -e "127.0.0.1 localhost\n::1 localhost\n127.0.1.1 david-danyal" >> /etc/hosts # Append something I don't understand to this file.
 
 (
@@ -25,14 +24,13 @@ pacman -S --noconfirm grub efibootmgr # Downloading and installing the boot load
 mkdir /boot/efi # Create a directory to mount the EFI partition onto
 mount /dev/vda1 /boot/efi # Mount the EFI partition to the specified directory
 
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot # Install the grub boot loader
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi # Install the grub boot loader
 grub-mkconfig -o /boot/grub/grub.cfg # Create the grub config file
 
-pacman -S --noconfirm xorg xfce4 xfce4-goodies lxdm # Download and install the xorg display server, the xfce4 desktop environment and the LXDM display manager
+pacman -S --noconfirm xorg # Download and install the xorg display server
+pacman -S --noconfirm xfce4 xfce4-goodies lxdm networkmanager # Download and install extra software
 
-systemctl enable NetworkManager.service
-systemctl start lxdm.service
-systemctl enable lxdm.service
+systemctl enable NetworkManager.service # Enable the networkmanager service with systemd
+systemctl enable lxdm.service # Enable the lxdm display manager service with systemd
 
-
-echo reboot | exit # Exit the chroot environment and reboot the system from the live ISO environment
+exit # Exit the chroot environment
